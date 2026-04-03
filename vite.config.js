b8dd4ac03164ccd58d626d81c7e18a9995ccbf87
Sujal More <sujalmore@Macbook-Air.local>
2026-04-03 15:59:05 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/snahrjs",
-  server: {
-    // Replace with your specific ngrok URL
-    allowedHosts: ["e302-2405-201-47-f9b1-1819-c47c-cfc0-1a3c.ngrok-free.app"],
-
-    // Optional: Set host to true to allow access from your local network
-    host: true,
-  },
+  base: "/snahrjs/",
 });
