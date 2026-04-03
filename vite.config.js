ed9f1e2ba03e7b7b92d3da5e439e5b91a6a73082
Sujal More <sujalmore@Macbook-Air.local>
2026-04-03 15:53:19 +0530

    changes for pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/snahrjs",
   server: {
     // Replace with your specific ngrok URL
-    allowedHosts: ['e302-2405-201-47-f9b1-1819-c47c-cfc0-1a3c.ngrok-free.app'], 
-    
+    allowedHosts: ["e302-2405-201-47-f9b1-1819-c47c-cfc0-1a3c.ngrok-free.app"],
+
     // Optional: Set host to true to allow access from your local network
-    host: true 
-  }
-})
+    host: true,
+  },
+});
